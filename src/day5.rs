558cf303b5d661a49e5b0c6094267231e87aef28
Patrick Barrett <patrickbarrett@exosite.com>
2015-12-09 10:28:27 -0600

    use proper regex library for day 5

--- src/day5.rs
+++ src/day5.rs
@@ -1,24 +1,9 @@
-extern crate regex;
-use regex::Regex;
+extern crate pcre;
+
+use pcre::Pcre;
+
 use std::io::prelude::*;
 use std::fs::File;
-
-use std::process::Command;
-
-fn has_repeats(name: &str) -> bool {
-    let mut hist = 0u8;
-
-    for byte in name.as_bytes() {
-        if hist == *byte {
-            return true;
-        }
-
-        hist = *byte;
-    }
-
-    false
-}
-
 
 fn main() {
     let mut input_file = File::open("input/day5.txt").unwrap();
@@ -28,39 +13,48 @@ fn main() {
     let mut good_list: Vec<&str> = Vec::new();
     let mut bad_list: Vec<&str>  = Vec::new();
 
-    let r1 = Regex::new(r"(:?[aeiou].*){3}").unwrap();
-    //let r2 = Regex::new(r"(:?[a-zA-Z])\1").unwrap();
-    let r3 = Regex::new(r"ab|cd|pq|xy").unwrap();
+    let mut r1 = Pcre::compile(r"(:?[aeiou].*){3}").unwrap();
+    let mut r2 = Pcre::compile(r"(:?[a-zA-Z])\1").unwrap();
+    let mut r3 = Pcre::compile(r"ab|cd|pq|xy").unwrap();
 
     for child in input.split("\n") {
-
-
-        if !r1.is_match(child) {
-            println!("{} breaks rule 1.", child);
+        if r1.exec(child).is_none() {
+            //println!("{} breaks rule 1.", child);
             bad_list.push(&child);
-        } else if !has_repeats(child) {
-            println!("{} breaks rule 2.", child);
+        } else if r2.exec(child).is_none() {
+            //println!("{} breaks rule 2.", child);
             bad_list.push(&child);
-        } else if r3.is_match(child) {
-            println!("{} breaks rule 3.", child);
+        } else if !r3.exec(child).is_none() {
+            //println!("{} breaks rule 3.", child);
             bad_list.push(&child);
         } else {
-            println!("{} is good.", child);
+            //println!("{} is nice.", child);
             good_list.push(&child);
         }
-
     }
 
-    println!("Good Children: {}", good_list.len());
-    println!("Bad Children: {}", bad_list.len());
+    println!("Nice Children: {}", good_list.len());
+    println!("Naughty Children: {}", bad_list.len());
 
-    // pt2 -- lazyness
-    // rust doesn't have backreferences in its regex.
-    let output = Command::new("sh").arg("-c")
-                       .arg("grep \"\\(..\\).*\\1\" input/day5.txt | grep \"\\(.\\).\\1\"")
-                       .output().unwrap();;
-    let output_str = String::from_utf8_lossy(&output.stdout);
-    let count = output_str.lines().count();
+    let mut good_list: Vec<&str> = Vec::new();
+    let mut bad_list: Vec<&str>  = Vec::new();
 
-    println!("Pt2 Count: {}", count);
+    let mut p2r1 = Pcre::compile(r"(..).*\1").unwrap();
+    let mut p2r2 = Pcre::compile(r"(.).\1").unwrap();
+
+    for child in input.split("\n") {
+        if p2r1.exec(child).is_none() {
+            //println!("{} breaks rule one. (pt2)", child);
+            bad_list.push(&child);
+        } else if p2r2.exec(child).is_none() {
+            //println!("{} breaks rule one. (pt2)", child);
+            bad_list.push(&child);
+        } else {
+            //println!("{} is nice. (pt2)", child);
+            good_list.push(&child);
+        }
+    }
+
+    println!("Nice Children: {} (pt2)", good_list.len());
+    println!("Naughty Children: {} (pt2)", bad_list.len());
 }
